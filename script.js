e39537f6e29e927e8d8952b1268b42a9d6dd365d
Mr <zack@labs.ws>
2020-01-14 17:41:59 +0200

    fixed some bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let appId = '0f94af7cedc0069968b5892d9d0c8b55';
-let units = 'metric';
-let searchMethod;
+let units = 'metric'; // other option is metric
+let searchMethod; // q means searching as a string.
 
 function getSearchMethod(searchTerm) {
     if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
@@ -10,59 +10,58 @@ function getSearchMethod(searchTerm) {
 }
 
 function searchWeather(searchTerm) {
-
     getSearchMethod(searchTerm);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=
-    ${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
-        return result.json();
-    }).then(result => {
-        init(result);
-    })
-
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+        });
 }
 
 function init(resultFromServer) {
     switch (resultFromServer.weather[0].main) {
         case 'Clear':
-            document.body.style.backgroundImage = 'url("clear.jpg")';
+            document.body.style.backgroundImage = "url('clear.jpg')";
             break;
 
         case 'Clouds':
-            document.body.style.backgroundImage = 'url("cloudy.jpg")';
+            document.body.style.backgroundImage = "url('cloudy.jpg')";
             break;
 
         case 'Rain':
         case 'Drizzle':
         case 'Mist':
-            document.body.style.backgroundImage = 'url("rain.jpg")';
+            document.body.style.backgroundImage = "url('rain.jpg')";
             break;
 
         case 'Thunderstorm':
-            document.body.style.backgroundImage = 'url("storm.jpg")';
+            document.body.style.backgroundImage = "url('storm.jpg')";
             break;
 
         case 'Snow':
-            document.body.style.backgroundImage = 'url("snow.jpg")';
+            document.body.style.backgroundImage = "url('snow.jpg')";
             break;
+
         default:
             break;
     }
+
     let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
     let temperatureElement = document.getElementById('temperature');
     let humidityElement = document.getElementById('humidity');
     let windSpeedElement = document.getElementById('windSpeed');
     let cityHeader = document.getElementById('cityHeader');
-    let weatherIcon = document.getElementById('documentIconImg');
 
+    let weatherIcon = document.getElementById('documentIconImg');
     weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
 
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
-
-    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
-    windSpeedElement.innerHTML = 'winds at' + Math.floor(resultFromServer.wind.speed) + 'm/s';
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176;';
+    windSpeedElement.innerHTML = 'Winds at  ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
     cityHeader.innerHTML = resultFromServer.name;
-    windSpeedElement.innerHTML = 'Humidity levels at' + resultFromServer.main.humidity + '%';
+    humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
 
     setPositionForWeatherInfo();
 }
@@ -81,4 +80,4 @@ document.getElementById('searchBtn').addEventListener('click', () => {
     let searchTerm = document.getElementById('searchInput').value;
     if (searchTerm)
         searchWeather(searchTerm);
-})
+});
